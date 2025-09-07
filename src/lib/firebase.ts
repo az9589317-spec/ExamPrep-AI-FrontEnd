56d4e7d8ff992b8d277525dd0b5dcda575290fc6
Rohan <az9589317@gmail.com>
2025-09-07 11:00:40 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: "examprep-ai-wi0xc",
-  appId: "1:598117662371:web:15119767a5cecbd4874916",
-  storageBucket: "examprep-ai-wi0xc.appspot.com",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "examprep-ai-wi0xc.firebaseapp.com",
-  messagingSenderId: "598117662371"
+  "projectId": "examprep-ai-wi0xc",
+  "appId": "1:598117662371:web:15119767a5cecbd4874916",
+  "storageBucket": "examprep-ai-wi0xc.appspot.com",
+  "apiKey": "AIzaSyAt7LdsUTDl-cyx1-kZvvmE7mlmNave-u8",
+  "authDomain": "examprep-ai-wi0xc.firebaseapp.com",
+  "messagingSenderId": "598117662371"
 };
 
 // Initialize Firebase
