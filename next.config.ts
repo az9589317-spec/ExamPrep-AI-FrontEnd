ceed8957572299fc82243e27c7eebe48eb5d1e2e
Rohan <az9589317@gmail.com>
2025-09-12 11:43:58 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'iili.io',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
